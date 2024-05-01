f88dde5cc73e94021f4cc981ab9aae2ebb65d192
cr1m3s <parioleks@gmail.com>
2024-05-01 13:38:17 +0200

    bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,14 @@
+terraform {
+  backend "gcs" {
+    bucket = "cr1m3s-example-1"
+    prefix = "terraform/state"
+  }
+}
+
 module "gke_cluster" {
   source         = "github.com/cr1m3s/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = var.GKE_NUM_NODES
 }
+
